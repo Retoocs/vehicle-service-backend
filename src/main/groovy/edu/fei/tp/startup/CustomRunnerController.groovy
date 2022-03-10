78fc094bcdbca068e1077a5832d47fca8d7f32c1
Matej <matejchvostek@gmail.com>
2022-03-10 09:19:43 +0100

    feat: add runners

--- src/main/groovy/edu/fei/tp/startup/CustomRunnerController.groovy
+++ src/main/groovy/edu/fei/tp/startup/CustomRunnerController.groovy
@@ -1,4 +1,4 @@
-package org.example.starter.startup
+package edu.fei.tp.startup
 
 import com.netgrif.application.engine.startup.*
 
@@ -21,17 +21,15 @@ class CustomRunnerController extends RunnerController {
             FlushSessionsRunner,
             MailRunner,
             PostalCodeImporter,
-            // CUSTOM IMPORT RUNNERS
-//TODO:     CUSTOM IMPORT RUNNER
-
-            // END OF CUSTOM IMPORT RUNNERS
             DemoRunner,
             QuartzSchedulerRunner,
             PdfRunner,
             FinisherRunnerSuperCreator,
             // ADDITIONAL CUSTOM RUNNERS
-//TODO:     CUSTOM RUNNERS
-            CustomRunner,
+            NetImportRunner,
+            CaseImportRunner,
+            CsvImportRunner,
+            VSFinisherRunner,
             // END OF ADDITIONAL CUSTOM RUNNERS
             FinisherRunner,
     ]
