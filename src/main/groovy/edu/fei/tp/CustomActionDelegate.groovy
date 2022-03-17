ca8123c88076e534141e509a657ee08950882de3
Matej <matejchvostek@gmail.com>
2022-03-17 11:27:13 +0100

    style: remove imports

--- src/main/groovy/edu/fei/tp/CustomActionDelegate.groovy
+++ src/main/groovy/edu/fei/tp/CustomActionDelegate.groovy
@@ -1,13 +1,8 @@
 package edu.fei.tp
 
-import com.netgrif.application.engine.auth.domain.RegisteredUser
-import com.netgrif.application.engine.auth.web.requestbodies.NewUserRequest
-import com.netgrif.application.engine.mail.interfaces.IMailService
 import com.netgrif.application.engine.petrinet.domain.dataset.logic.action.ActionDelegate
-import freemarker.template.TemplateException
 import org.springframework.stereotype.Component
 
-import javax.mail.MessagingException
 
 @Component
 class CustomActionDelegate extends ActionDelegate {
