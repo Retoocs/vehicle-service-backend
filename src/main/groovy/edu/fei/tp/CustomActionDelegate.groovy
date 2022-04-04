3f5d10fcf69af3ecf635e215d9a5812e93c736ef
Matej <matejchvostek@gmail.com>
2022-04-04 21:54:00 +0200

    feat: small refactor of vehicles import

--- src/main/groovy/edu/fei/tp/CustomActionDelegate.groovy
+++ src/main/groovy/edu/fei/tp/CustomActionDelegate.groovy
@@ -1,22 +1,29 @@
 package edu.fei.tp
 import com.netgrif.application.engine.petrinet.domain.dataset.EnumerationMapField
 import com.netgrif.application.engine.petrinet.domain.dataset.logic.action.ActionDelegate
+import edu.fei.tp.helpers.CSVHelper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 
 @Component
 class CustomActionDelegate extends ActionDelegate {
-    String path = "cars.csv";
-    String splitter = ",";
+    public static final Logger log = LoggerFactory.getLogger(CustomActionDelegate.class)
 
-    public Map getCars(String splitter) {
-        CSVHelper helper = new CSVHelper()
-        def csv= helper.loadCSV(path)
+    static private String pathToCarsCsv = "src/main/resources/csv/cars.csv"
+    static private String csvSplitter = ","
+
+    private static Map getCars(String splitter) {
+        def csv = CSVHelper.loadCSV(pathToCarsCsv)
         def cars = [:]
-        csv.each {s ->
+        csv.eachWithIndex { s, idx ->
+            if(idx == 0)
+                return
+
             def vehiclesArr = s.split(splitter)
-            def manufacturer=vehiclesArr[0].replaceAll("\\.", "-");
-            def model=vehiclesArr[1].replaceAll("\\.", "-");
+            def manufacturer = vehiclesArr[0].replaceAll("\\.", "-");
+            def model = vehiclesArr[1].replaceAll("\\.", "-");
 
             if(cars.containsKey(manufacturer)){
                 if(!cars[manufacturer].contains(model)){
@@ -26,19 +33,21 @@ class CustomActionDelegate extends ActionDelegate {
             else{
                 cars[manufacturer] = []
                 cars[manufacturer].add(model)
-                }
             }
-        return cars;
+        }
+        return cars
     }
 
 
-    public void setCars(EnumerationMapField make,EnumerationMapField model){
-        def cars = getCars(splitter)
+    void setCars(EnumerationMapField manufacturers, EnumerationMapField models){
+        def cars = getCars(csvSplitter)
         for (entry in cars) {
-            change make options {   make.options + [(entry.getKey()):entry.getKey()];  }
+            change manufacturers options {   manufacturers.options + [(entry.getKey()):entry.getKey()];  }
             for(mod in entry.getValue()){
-                change model options {  model.options + [(entry.getKey()+"-"+mod):mod];  }
+                change models options {  models.options + [(entry.getKey()+"-"+mod):mod];  }
             }
         }
+
+        log.info("Succesfully loaded " + models.options?.size() + " vehicles into " + useCase.petriNet.title)
     }
 }
