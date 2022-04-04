dcad476d61e2c39508e7f6b3462b3c70cda2db4a
simon <sajmoooonsk@gmail.com>
2022-04-04 12:11:39 +0200

    feat: add manufacturers and models to options

--- src/main/groovy/edu/fei/tp/CustomActionDelegate.groovy
+++ src/main/groovy/edu/fei/tp/CustomActionDelegate.groovy
@@ -1,9 +1,44 @@
 package edu.fei.tp
-
+import com.netgrif.application.engine.petrinet.domain.dataset.EnumerationMapField
 import com.netgrif.application.engine.petrinet.domain.dataset.logic.action.ActionDelegate
 import org.springframework.stereotype.Component
 
 
 @Component
 class CustomActionDelegate extends ActionDelegate {
+    String path = "cars.csv";
+    String splitter = ",";
+
+    public Map getCars(String splitter) {
+        CSVHelper helper = new CSVHelper()
+        def csv= helper.loadCSV(path)
+        def cars = [:]
+        csv.each {s ->
+            def vehiclesArr = s.split(splitter)
+            def manufacturer=vehiclesArr[0].replaceAll("\\.", "-");
+            def model=vehiclesArr[1].replaceAll("\\.", "-");
+
+            if(cars.containsKey(manufacturer)){
+                if(!cars[manufacturer].contains(model)){
+                    cars[manufacturer].add(model)
+                }
+            }
+            else{
+                cars[manufacturer] = []
+                cars[manufacturer].add(model)
+                }
+            }
+        return cars;
+    }
+
+
+    public void setCars(EnumerationMapField make,EnumerationMapField model){
+        def cars = getCars(splitter)
+        for (entry in cars) {
+            change make options {   make.options + [(entry.getKey()):entry.getKey()];  }
+            for(mod in entry.getValue()){
+                change model options {  model.options + [(entry.getKey()+"-"+mod):mod];  }
+            }
+        }
+    }
 }
