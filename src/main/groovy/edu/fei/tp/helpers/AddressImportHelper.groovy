137b890683d44aef41da3d05dd369b762be61c1a
simon <sajmoooonsk@gmail.com>
2022-04-26 14:50:07 +0200

    feat: edit csvhelper

--- src/main/groovy/edu/fei/tp/helpers/AddressImportHelper.groovy
+++ src/main/groovy/edu/fei/tp/helpers/AddressImportHelper.groovy
@@ -13,10 +13,9 @@ class AddressImportHelper {
         }
         def csv = CSVHelper.loadCSV(file)
 
-        String headerRow = csv[0].toLowerCase()
+        String headerRow = csv.get(0).toLowerCase()
         String[] values;
         values = headerRow.split(',');
-        println(values)
 
         if (!(values.length == 3)) {
             return []
